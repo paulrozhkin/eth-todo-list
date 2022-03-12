936bd5762c7bd4698bd71a7bc7cb92e40171c4b7
Paul Rozhkin <rozhkin@protei.ru>
2022-03-12 21:10:56 +0300

    complete tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -27,4 +27,11 @@ contract TodoList {
         tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
     }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCreated(_task.id, _task.content, _task.completed);
+    }
 }
